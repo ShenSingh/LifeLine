3e5bf38b7d82c5b20d376a524561e9921444dfde
ShenSingh <gayashan9090@gmail.com>
2025-03-13 06:13:28 +0530

    renew-frontend

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../component/Navbar';
+import Hero from '../component/Hero';
+import HomeStatistic from '../component/HomeStatistic';
+export const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <HomeStatistic />
+        </>
+    );
+};
