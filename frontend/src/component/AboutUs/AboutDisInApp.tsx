ad2fd2ef9ddd94ae3547c12238b5ba913d70d66e
ShenSingh <gayashan9090@gmail.com>
2025-03-14 07:44:16 +0530

    frontend-create

---
+++ frontend/src/component/AboutUs/AboutDisInApp.tsx
@@ -0,0 +1,28 @@
+export default function AboutDisInApp() {
+    return (
+        <div className="flex flex-col justify-between w-full px-4 mx-auto lg:pt-16 lg:flex-row md:px-8 lg:w-full bg-white">
+            <div className="pt-16 mb-16 lg:mb-0 lg:pt-32 lg:max-w-lg lg:pr-5">
+                <div className="max-w-xl mb-6">
+                    <div>
+                        <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                            Brand new
+                        </p>
+                    </div>
+                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                        අපගේ රුධිර පරිත්‍යාග සෙවුම් වෙබ් අඩවිය
+                    </h2>
+                    <p className="text-base text-gray-700 md:text-lg">
+                        අපගේ රුධිර පරිත්‍යාග සෙවුම් වෙබ් අඩවිය, පරිත්‍යාගශීලීන්ගේ සහ ලබන්නන්ගේ අනන්‍ය අවශ්‍යතා සපුරාලීම සඳහා සකස් කරන ලද විශේෂාංග මාලාවක් ඉදිරිපත් කිරීමෙන් පරිශීලක පහසුව සඳහා ප්‍රමුඛතාවය දෙයි. පරිත්‍යාගශීලීන්ට වේදිකාවේ ලියාපදිංචි විය හැකි අතර, ඔවුන්ගේ රුධිර වර්ගය සහ සම්බන්ධතා තොරතුරු සහාය පතන අයට පහසුවෙන් ලබා ගත හැකිය. මෙම ක්‍රියාශීලී ප්‍රවේශය හදිසි අවස්ථා වලදී කඩිනම් ප්‍රතිචාරයක් සහතික කරයි. මීට අමතරව, වෙබ් අඩවිය හදිසි රුධිර ඉල්ලීම් පිළිබඳව පරිත්‍යාගශීලීන් දැනුවත් කිරීම සඳහා දැනුම් දීමේ පද්ධතියක් භාවිතා කරයි, සහභාගිවන්නන් අතර ප්‍රජාව සහ සහයෝගීතාවය පිළිබඳ හැඟීමක් ඇති කරයි. මෙම සම්බන්ධිත ජාලය පෝෂණය කිරීමෙන්, ශ්‍රී ලංකාව පුරා රුධිර පරිත්‍යාගය බාධාවකින් තොරව සහ කාර්යක්ෂම ක්‍රියාවලියක් බවට පත් කිරීම අපගේ අරමුණයි.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img
+                    src="https://kitwind.io/assets/kometa/two-thirds-phone.png"
+                    className="object-cover object-top w-full h-64 mx-auto lg:h-auto xl:mr-24 md:max-w-sm"
+                    alt="Blood Donation Illustration"
+                />
+            </div>
+        </div>
+    );
+}
